760a4a3bfc6c627ce84a518d0b9407ff9cf933ec
lijing <850237064@qq.com>
2018-07-17 16:42:02 +0800

    1

--- Application/Api/Controller/GoodsController.class.php
+++ Application/Api/Controller/GoodsController.class.php
@@ -21,8 +21,6 @@ class GoodsController extends Controller {
         
         if(!$list){
             returnjson('400','fail','数据查询失败');
-        }else if(count($list)==0){
-            returnjson('200','success','暂无相关数据');
         }else{
             returnjson('200','success',$list);
         }
--- Application/Api/Controller/GoodtypeController.class.php
+++ Application/Api/Controller/GoodtypeController.class.php
@@ -8,9 +8,7 @@ class GoodtypeController extends Controller {
 	public function index(){
         $list = M('good_type')->order('orderby_id asc')->select();
         if(!$list){
-            returnjson('400','fail','数据查询失败');
-        }else if(count($list)==0){
-            returnjson('200','success','暂无相关数据');
+            returnjson('400','fail','数据查询失败,暂无相关数据');
         }else{
             returnjson('200','success',$list);
         }
