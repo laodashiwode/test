4516245c578017719ea151e2b147470aa17b3ee6
lee <850237064@qq.com>
2018-08-09 20:26:07 +0800

    修改

--- Application/Api/Controller/GoodsController.class.php
+++ Application/Api/Controller/GoodsController.class.php
@@ -16,6 +16,7 @@ class GoodsController extends Controller {
         $count      = $goods->where('type_id ='.$type_id)->count();// 查询满足要求的总记录数
         $Page       = new \Think\Page($count,10);// 实例化分页类 传入总记录数和每页显示的记录数(25)
         $show       = $Page->show();// 分页显示输出
+        
         // 进行分页数据查询 注意limit方法的参数要使用Page类的属性
         $list = $goods->where('type_id ='.$type_id)->limit($Page->firstRow.','.$Page->listRows)->select();
         
